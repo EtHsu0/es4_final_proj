878174be00a3a663c952a6e99891d1a699cbd971
xertyx <ethan197019@gmail.com>
2022-12-03 13:28:03 -0500

    Update_randomPos

--- ES4_Snake/source/impl_1/randomPos.vhd
+++ ES4_Snake/source/impl_1/randomPos.vhd
@@ -3,7 +3,7 @@ use IEEE.math_real.uniform;
 use IEEE.math_real.floor;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-use IEEE.std_logic_arith.all;
+-- use IEEE.std_logic_arith.all;
 
 entity randomPos is
         port (
@@ -11,18 +11,19 @@ entity randomPos is
                 clk: in std_logic;
                 coord: in unsigned(7 downto 0);
                 bound: in unsigned(10 downto 0);
+                out_coord: out unsigned(7 downto 0)
         );
 end randomPos;
 
-architecture synth of top is
+architecture synth of randomPos is
 signal seed1: positive := 1;
 signal seed2: positive := 2;
 signal gen: real;
-signal int: integer;
+signal int: unsigned(7 downto 0);
 begin
     process is begin
         uniform(seed1, seed2, gen);
-        int := integer(floor(gen * bound));
-        
+        int := unsigned(floor(gen * bound));
+
         end process;
 end;
