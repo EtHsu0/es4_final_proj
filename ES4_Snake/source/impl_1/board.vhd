bd7e878eee764d26f0a301dcee3529592df157e3
xertyx <ethan197019@gmail.com>
2022-12-04 15:58:51 -0500

    Update

--- ES4_Snake/source/impl_1/board.vhd
+++ ES4_Snake/source/impl_1/board.vhd
@@ -7,8 +7,8 @@ use IEEE.numeric_std.all;
 
 entity board is
         port (
-			clk: in std_logic;
-			data: in std_logic_vector(7 downto 0);
+            clk: in std_logic;
+            data: in std_logic_vector(7 downto 0);
             -- Return the cell ID (0-99)
             apple_id: out unsigned(7 downto 0);
             -- Return whether snake is in each cell (0-99)
@@ -18,22 +18,24 @@ end board;
 
 architecture synth of board is
 component randomPos is
-	port (
-		enable: in std_logic;
-		clk: in std_logic;
-		coord: in unsigned(7 downto 0);
-		bound: in unsigned(10 downto 0);
-		out_coord: out unsigned(7 downto 0)
-	);
+    port (
+        enable: in std_logic;
+        clk: in std_logic;
+        coord: in unsigned(7 downto 0);
+        bound: in unsigned(10 downto 0);
+        out_coord: out unsigned(7 downto 0)
+    );
 end component;
 
-signal apple: unsigned (7 downto 9);
-signal snakePosition: unsigned(99 downto 0);
+signal apple: unsigned (7 downto 9) := ;
+signal snakePosition: unsigned(99 downto 0) := (40 => '1', 41 => '1', 42 => '1'),
+                                                (others => '0);
 
 begin
-	-- Update the snake head position, use that information to check collision with apple
-		-- If collieded with apple, set a state and generate anoother apple.
-	-- snakePos -> Get updated snake position based on the new head position
-	-- Check collision with body / wall based on the new snake position
-	
+    
+    -- Update the snake head position, use that information to check collision with apple
+        -- If collieded with apple, set a state and generate anoother apple.
+    -- snakePos -> Get updated snake position based on the new head position
+    -- Check collision with body / wall based on the new snake position
+    
 end;
--- ES4_Snake/source/impl_1/topModule.vhd
+++ ES4_Snake/source/impl_1/topModule.vhd
@@ -4,43 +4,43 @@ use IEEE.numeric_std.all;
 
 entity top is 
     port(
-        -- NES controllers input
-		data: in std_logic;
-		
-        -- VGA display output
-		pll_in_clock : in std_logic; -- pin 20, shorted to the 12 MHz pin on the UPduino
-		pll_outcore_o : out std_logic; -- for testing purposes (pin 2)
-		
-		HSYNC : out std_logic; -- pin 23
-		VSYNC : out std_logic; -- pin 25
-		
-		rgb : out unsigned(5 downto 0) -- pin 28, 38, 42, 36, 43, and 34
+        -- NES controllers IO
+        data: in std_logic;
+        
+        -- VGA display IO
+        pll_in_clock : in std_logic; -- pin 20, shorted to the 12 MHz pin on the UPduino
+        
+        HSYNC : out std_logic; -- pin 23
+        VSYNC : out std_logic; -- pin 25
+        
+        rgb : out unsigned(5 downto 0) -- pin 28, 38, 42, 36, 43, and 34
+
+        pll_outcore_o : out std_logic; -- for testing purposes (pin 2)
+
     );
 end top;
 
 architecture synth of top is
-component HSOSC is
-    generic (
-        CLKHF_DIV : String := "0b00"); --divide clock by 2^N (0-3)
-    port (
-        CLKHFPU : in std_logic := 'X'; -- Set to 1 to power up
-        CLKHFEN : in std_logic := 'X'; --Set to 1 to enable output
-        CLKHF : out std_logic := 'X'); --Clock output
-end component;
+    component HSOSC is
+        generic (
+            CLKHF_DIV : String := "0b00"); --divide clock by 2^N (0-3)
+        port (
+            CLKHFPU : in std_logic := 'X'; -- Set to 1 to power up
+            CLKHFEN : in std_logic := 'X'; --Set to 1 to enable output
+            CLKHF : out std_logic := 'X'); --Clock output
+    end component;
 
-component NES is
-    port (
-        CLK: in std_logic; -- Added by Ethan to take clock input
-        data : in std_logic;
+    component NES is
+        port (
+            CLK: in std_logic; -- Added by Ethan to take clock input
+            data : in std_logic;
 
-        latch : out std_logic;
-        continCLK : out std_logic;
-        digital : out unsigned(7 downto 0)
-    );
-end component;
+            latch : out std_logic;
+            continCLK : out std_logic;
+            digital : out unsigned(7 downto 0)
+        );
+    end component;
 
--- Components for VGA
--- Components for NES?
 -- Components for Snake
 -- Components for menu selection?
 -- Components for setting options
@@ -53,21 +53,21 @@ end component;
     -- Other "sub" states for each options
     -- For example after start, we have ongoing, game over, etc.
 
-signal CLK: std_logic;
-signal latch: std_logic;
-signal continCLK: std_logic;
-signal digital: unsigned(7 downto 0);
+    signal CLK: std_logic;
+    signal latch: std_logic;
+    signal continCLK: std_logic;
+    signal digital: unsigned(7 downto 0);
 
 begin
     HSOSC_instance : HSOSC port map
                             (CLKHFPU => '1', CLKHFEN => '1', CLKHF => CLK);
     
-	NES_instance: NES port map (CLK, data, latch, continCLK, digital);
-	
-	-- display_instance: display port map ();
-	
-	
-	
+    NES_instance: NES port map (CLK, data, latch, continCLK, digital);
+    
+    -- display_instance: display port map ();
+    
+    
+    
 
 --     a <= '1';
 end;
