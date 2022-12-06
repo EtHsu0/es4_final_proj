dbd2e16d6eb991585858391b13bf0814d2c68ffc
xertyx <ethan197019@gmail.com>
2022-12-06 13:13:44 -0500

    Last_night_wtv

--- ES4_Snake/source/impl_1/board.vhd
+++ ES4_Snake/source/impl_1/board.vhd
@@ -1,6 +1,4 @@
 library IEEE;
-use IEEE.math_real.uniform;
-use IEEE.math_real.floor;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 -- use IEEE.std_logic_arith.all;
@@ -8,19 +6,19 @@ use IEEE.numeric_std.all;
 entity board is
     port (
         clk: in std_logic;
-
         -- 00 is RESET/BEGIN
-        game_state: in unsigned(1 downto 0) := "00";
+        game_state: out unsigned(1 downto 0) := "00";
 
         digital_in: in unsigned(7 downto 0);
 
-        snake_head_out: out unsigned(7 downto 0);
+        snake_head_out: out unsigned(6 downto 0);
         -- Return the cell ID (0-99)
         apple_out: out unsigned(8 downto 0);
+        
         -- Return whether snake is in each cell (0-99)
         snake_arr_out: out std_logic_vector(99 downto 0);
 
-        scores_out: out unsigned(7 downto 0) := 8b"0"
+        scores_out: out unsigned(6 downto 0)
     );
 end board;
 
@@ -31,10 +29,8 @@ architecture synth of board is
             reset: in std_logic;
     
             grow_snake: in std_logic;
-    
-            -- Direction should be 0-3 (Up, Down, Left, Right)
+
             dir_in: in unsigned(1 downto 0); 
-            -- Gabriel Note: dirtype is not defined, need to fix dirtype declaration or remove entirely
             -- 00 = up
             -- 01 = down
             -- 10 = left
@@ -55,6 +51,7 @@ architecture synth of board is
     end component;
 
     signal apple_id: unsigned (7 downto 0) := 8d"0";
+    signal snake_head: unsigned (6 downto 0);
     signal snake_array: std_logic_vector(99 downto 0); -- := (40 => '1', 41 => '1', 42 => '1'),(others => '0'); -- TODO: Fix Syntax Error!
     signal counter: unsigned (29 downto 0) := 30d"0";
     signal reset: std_logic := '1';
@@ -83,37 +80,27 @@ begin
            "10" when digital_in(1) = '0' else
            "11";
 
-    -- reset <= '1' when game_state = "00";
-    process is begin
-        -- if reset = '1' then
-        snake_arr_out <= 100d"14";
-        snake_arr_out(42) <= '1';
-        snake_arr_out(43) <= '1';
-        snake_arr_out(44) <= '1';
-        -- end if;
-    end process;
-    -- snake_arr_out <= (others => '0');
-    -- snake_arr_out(42) <= '1';
-    -- snake_arr_out(43) <= '1';
-    -- snake_arr_out(44) <= '1';
-    -- snakePos_inst: snakePos port map (counter(29),reset,growSnake,dir,snake_head_out,snake_arr_out,snake_dead);
+    snakePos_inst: snakePos port map (counter(29),reset,growSnake,dir,snake_head,snake_arr_out,snake_dead);
 
-    -- apple_random: randomPos port map (enable, clk, apple_out);
+    apple_random: randomPos port map (enable, clk, apple_out);
 
     -- Check collision with apple
-    -- process is begin
-    --     -- If snake head is on apple's coordinate
-    --     if snake_head_out = apple_out then
-    --         -- Create artifical rising edge for enable signal
-    --         enable <= '0';
-    --         wait for 5 ns;
-    --         enable <= '1';
-    --         -- Create this signal until we generate a valid apple id (apple can not spawn on snake)
-    --         --while (snake(apple_out) = '1' or apple_out = snake_head_out) loop
-    --             --enable <= '0';
-    --             --wait for 5 ns;
-    --             --enable <= '1';
-    --         --end loop;
-    --     end if;
-    -- end process;
+    process (snake_head) is begin
+        -- If snake head is on apple's coordinate
+        if snake_head_out = apple_out then
+            -- Create artifical rising edge for enable signal
+            enable <= '0';
+            wait for 5 ns;
+            enable <= '1';
+            -- Create this signal until we generate a valid apple id (apple can not spawn on snake)
+            --while (snake(apple_out) = '1' or apple_out = snake_head_out) loop
+                --enable <= '0';
+                --wait for 5 ns;
+                --enable <= '1';
+            --end loop;
+        end if;
+    end process;
+    game_state <= "00";
+    scores_out <= "0";
+    snake_head_out <= snake_head;
 end;
--- ES4_Snake/source/impl_1/topModule.vhd
+++ ES4_Snake/source/impl_1/topModule.vhd
@@ -1,6 +1,6 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
 entity top is 
     port(
@@ -56,81 +56,51 @@ architecture synth of top is
     
             -- Game logic
             apple: in unsigned(8 downto 0);
-            snake: in std_logic_vector(99 downto 0)
+            snake_head: in unsigned(6 downto 0);
+            snake: in std_logic_vector(99 downto 0);
+            scores: in unsigned(6 downto 0);
+            game_state: in unsigned(1 downto 0)
         );
     end component;
 
     component board is
         port (
             clk: in std_logic;
-			game_status: unsigned(1 downto 0);
+            game_state: out unsigned(1 downto 0);
             digital: in unsigned(7 downto 0);
 
-            snake_head: out unsigned(7 downto 0);
+            snake_head: out unsigned(6 downto 0);
             -- Return the cell ID (0-99)
-            apple_id: out unsigned(7 downto 0);
+            apple_id: out unsigned(6 downto 0);
             -- Return whether snake is in each cell (0-99)
             snake: out std_logic_vector(99 downto 0);
 
-            scores: out unsigned(7 downto 0) := 8b"0"
-        );
+            scores: out unsigned(6 downto 0)
+            );
     end component;
-    
--- Components for Snake
--- Components for menu selection?
--- Components for setting options
--- Components for random apples generation 
-    -- (Maybe component will be called in another component)
-    -- Return different combinations of settings.
-    -- Bitpacking information into 
--- State we have? Initial state (Menu selection)
-    -- One state for each menu options (Start, Settings, End)
-    -- Other "sub" states for each options
-    -- For example after start, we have ongoing, game over, etc.
 
     signal CLK: std_logic;
     
     signal digital: unsigned(7 downto 0);
-    signal snake_head: unsigned(7 downto 0);
-    signal garbage_apple: unsigned(7 downto 0);
+
+    signal gameState: unsigned(1 downto 0) := "00";
+
+    signal snake_head: unsigned(6 downto 0);
+    signal apple_id: unsigned(6 downto 0);
     signal apple: unsigned(8 downto 0);
     signal snake: std_logic_vector(99 downto 0);
-    signal scores: unsigned(7 downto 0);
-    signal direction: unsigned(1 downto 0);
-
-    -- signal test_snake: 
-
-    TYPE STATE is (START, RUNNING, OVER);
-    signal gameState: STATE := START;
+    signal scores: unsigned(6 downto 0) := 7d"0";
 begin
     HSOSC_inst : HSOSC port map
                             (CLKHFPU => '1', CLKHFEN => '1', CLKHF => CLK);
     
     NES_inst: NES port map (CLK, data, latch, continCLK, digital);
     
-    -- Logics to convert NES digital output to buttons.
+    board_inst: board port map (clk, gameState, digital, snake_head, apple, snake, scores);
 
+    display_inst: display port map (pll_in_clock, pll_outcore_o, HSYNC, VSYNC, rgb, apple, snake_head, snake, scores, gameState);
 
-    board_inst: board port map (clk, 2b"00", digital, snake_head, apple, snake, scores);
-
-    
-    apple <= 9b"1_1000_0111" when digital = "11101111" else
-                9b"1_0000_0111";
-
-    -- process is begin
-    --     snake <= 100d"14";
-    --     snake(44) <= '1';
-    --     snake(43) <= '1';
-    --     snake(42) <= '1';
-    -- end process;
-
-    -- snake <= snake(99 downto 1) & "11";    --snake(43) <= '1';
-    -- snake <= (44 => '1');
-
-    display_inst: display port map (pll_in_clock, pll_outcore_o, HSYNC, VSYNC, rgb, apple, snake);
-	
 	delete_me <= digital(2 downto 0);
-
 end;
 
 
