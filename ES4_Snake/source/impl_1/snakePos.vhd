7b85156ed7c907dec486d213311754c644cc6cf2
xertyx <ethan197019@gmail.com>
2022-12-06 23:17:29 -0500

    snake_push

--- ES4_Snake/source/impl_1/snakePos.vhd
+++ ES4_Snake/source/impl_1/snakePos.vhd
@@ -67,33 +67,33 @@ begin
             slow_test_counter <= slow_test_counter + 1;
             if reset_in = '1' then
                 slow_test_counter <= 7d"0";
-                arr_left <= 8d"96";
-                arr_right <= 8d"99";
-                snake_head <= 8d"44";
-                dir_arr <= (others => RIGHT);
+                -- arr_left <= 8d"96";
+                -- arr_right <= 8d"99";
+                -- snake_head <= 8d"44";
+                -- dir_arr <= (others => RIGHT);
                 snake_arr <= (44 downto 42 => '1', others => '0');
 			end if;
                 -- for i in 99 downto 0 loop
                                 --     snake_arr(i) <= '0';
                 -- end loop;
         -- Check direction is valid
-            if prev_dir(1) /= dir_in(1) then
-                prev_dir <= dir_in;
-            end if;
+            -- if prev_dir(1) /= dir_in(1) then
+            --     prev_dir <= dir_in;
+            -- end if;
         --     -- Convert to type
-            case prev_dir is
+            case dir_in is
                 when "00" => snake_dir <= UP;
                 when "01" => snake_dir <= DOWN;
                 when "10" => snake_dir <= LEFT;
                 when "11" => snake_dir <= RIGHT;
             end case;
 
-            case snake_dir is
-                when UP => snake_arr <= (3 downto 0 => '1', others => '0');
-                when DOWN => snake_arr <= (13 downto 10 => '1', others => '0');
-                when LEFT => snake_arr <= (23 downto 20 => '1', others => '0');
-                when RIGHT => snake_arr <= (33 downto 30 => '1', others => '0');
-            end case;
+            -- case snake_dir is
+            --     when UP => snake_arr <= (3 downto 0 => '1', others => '0');
+            --     when DOWN => snake_arr <= (13 downto 10 => '1', others => '0');
+            --     when LEFT => snake_arr <= (23 downto 20 => '1', others => '0');
+            --     when RIGHT => snake_arr <= (33 downto 30 => '1', others => '0');
+            -- end case;
         --     -- Remove / update tail if we are not growing
         --     if grow_snake_in = '0' then
         --         arr_right <= arr_right - 1;
