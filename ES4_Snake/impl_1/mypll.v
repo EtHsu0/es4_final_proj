20728651f9aedcb9e5d495085c711d2f6b9c1285
Gabriel Sessions <gsess7732@gmail.com>
2022-12-07 23:19:52 -0500

    expanding

--- ES4_Snake/impl_1/mypll.v
+++ ES4_Snake/impl_1/mypll.v
@@ -1,5 +1,5 @@
 // Verilog netlist produced by program LSE 
-// Netlist written on Wed Dec  7 22:46:01 2022
+// Netlist written on Wed Dec  7 22:55:21 2022
 // Source file index table: 
 // Object locations will have the form @<file_index>(<first_ line>[<left_column>],<last_line>[<right_column>])
 // file 0 "c:/lscc/radiant/3.2/ip/avant/fifo/rtl/lscc_fifo.v"
