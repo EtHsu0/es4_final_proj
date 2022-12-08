fb89c942cb6ef9f4d2cafa5e696b7835f4787a0b
xertyx <ethan197019@gmail.com>
2022-12-08 04:52:14 -0500

    change port

--- ES4_Snake/source/impl_1/display.vhd
+++ ES4_Snake/source/impl_1/display.vhd
@@ -3,21 +3,22 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity display is
-port(
-    pll_in_clock : in std_logic; -- pin 20, shorted to the 12 MHz pin on the UPduino
-    pll_outcore_o : out std_logic; -- pin 21, for testing purposes
+    port(
+        pll_in_clock : in std_logic; -- pin 20, shorted to the 12 MHz pin on the UPduino
+        pll_outcore_o : out std_logic; -- pin 21, for testing purposes
 
-    HSYNC : out std_logic; -- pin 46
-    VSYNC : out std_logic; -- pin 2
-    rgb : out unsigned(5 downto 0); -- pins 47, 45, 48, 3, 4, 44
+        HSYNC : out std_logic; -- pin 46
+        VSYNC : out std_logic; -- pin 2
+        
+        rgb : out unsigned(5 downto 0); -- pins 47, 45, 48, 3, 4, 44
 
-    -- Game logic
-    apple: in unsigned(8 downto 0);
-    snake: in std_logic_vector(99 downto 0);
-
-    -- SCORE
-    game_score : in unsigned(5 downto 0)
-);
+        -- Game logic
+        apple: in unsigned(8 downto 0);
+        snake_head: in unsigned(6 downto 0);
+        snake: in std_logic_vector(99 downto 0);
+        scores: in unsigned(6 downto 0);
+        game_state: in unsigned(1 downto 0)
+    );
 end display;
 
 architecture synth of display is
@@ -42,15 +43,18 @@ port(
 end component;
 
 component pattern_gen is
-port(
-    valid : in std_logic; -- when valid is '0', the VGA's RGB output must be all 0's
-    y_pos : in unsigned(9 downto 0); -- the current y position (row count) of pixel that the VGA is drawing
-    x_pos : in unsigned(9 downto 0); -- the current x position (column count) of pixel that the VGA is drawing
-    rand_apple : in unsigned(8 downto 0);
-    snake_location : in std_logic_vector(99 downto 0);
-    score : in unsigned(5 downto 0);
-    rgb : out unsigned(5 downto 0) -- color of the individual pixel that the VGA is drawing
-);
+	port(
+		valid : in std_logic; -- when valid is 0, all RGB output must be low
+		y_pos : in unsigned(9 downto 0); -- current y position (row) of the pixel that the VGA is drawing
+		x_pos : in unsigned(9 downto 0); -- current x position (column) of the pixel that the VGA is drawing
+		rand_apple : in unsigned(8 downto 0);
+		snake_location : in std_logic_vector(99 downto 0);
+        score : in unsigned(5 downto 0);
+		rgb : out unsigned(5 downto 0);
+        snake_head: in unsigned(6 downto 0) := "0010000";
+        scores: in unsigned(6 downto 0);
+        game_state: in unsigned(1 downto 0)
+	);
 end component;
 
 component eight_segments is
@@ -83,13 +87,24 @@ begin
     -- 9B"1_1000_0111" represents the random apple. "1" means that it exists, "1000" means that the column number is 8, "0111" means that the row number is 7
     --snake_loc <= ("0000000000000000001000000000100000000010111111111010000000000000000000000000000000000000000000000000"); 
     --pattern_gen_init: pattern_gen port map(valid, row_cnt, column_cnt, 9B"1_1000_0111", snake_loc, rgb);
-    pattern_gen_initial: pattern_gen port map(valid, row_cnt, column_cnt, apple, snake, "00001", intermed_rgb);
+    pattern_gen_initial: pattern_gen port map(
+        valid => valid, 
+        y_pos => row_cnt, 
+        x_pos => column_cnt, 
+        rand_apple => apple, 
+        snake_location => snake, 
+        score => scores, 
+        rgb => intermed_rgb,
+        snake_head => snake_head,
+        game_state => game_state);
+    
+    
     segments: eight_segments port map(row_cnt, column_cnt, clk, score_rgb);
 
     rgb <= intermed_rgb when (column_cnt > 50 and valid = '1') else 
     score_rgb when (column_cnt < 50 and valid = '1') else 
     "000000";
 
-    
+
 
 end;
