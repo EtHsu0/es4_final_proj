05ea2bd119a1262dfb9b7618770bd2188e1103c4
xertyx <ethan197019@gmail.com>
2022-12-08 01:56:09 -0500

    comit

--- ES4_Snake/source/impl_1/board.vhd
+++ ES4_Snake/source/impl_1/board.vhd
@@ -80,8 +80,8 @@ begin
                     game_state <= "00";
                 end if;
             end if;
-           apple_y <= (apple_coord mod 4d"10")
-           apple_x <= (apple_coord / 4d"10")
+           apple_y <= (apple_coord mod 4d"10");
+           apple_x <= (apple_coord / 4d"10");
 
             apple_id <= '1' & apple_x & apple_y;
         end if;
