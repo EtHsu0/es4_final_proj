9617a3ed742e35fdf5e6bff559e4f854dc151f9b
Gabriel Sessions <gsess7732@gmail.com>
2022-12-08 02:58:14 -0500

    semicolon

--- ES4_Snake/source/impl_1/board.vhd
+++ ES4_Snake/source/impl_1/board.vhd
@@ -89,7 +89,7 @@ begin
     end process;
 
     apple_x <= (apple_coord mod 4d"10");
-    apple_y_inter <= (apple_coord * 7d"52")
+    apple_y_inter <= (apple_coord * 7d"52");
     apple_y <= apple_coord / 4d"10";--apple_y_inter(12 downto 9);
     apple_id <= '1' & apple_x & apple_y;
 
