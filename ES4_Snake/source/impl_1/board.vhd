e4ffade8ce0cacde14bd442c02ef1a156bc45c19
xertyx <ethan197019@gmail.com>
2022-12-08 01:57:14 -0500

    update apple

--- ES4_Snake/source/impl_1/board.vhd
+++ ES4_Snake/source/impl_1/board.vhd
@@ -73,17 +73,16 @@ begin
                     -- game_state <= "10";
                 end if;
                 apple_coord <= 0;
-
             elsif game_state <= "10" then
                 apple_coord <= 99;
                 if digital_in(5) = '0' then
                     game_state <= "00";
                 end if;
             end if;
-           apple_y <= (apple_coord mod 4d"10");
-           apple_x <= (apple_coord / 4d"10");
+        --    apple_y <= (apple_coord mod 4d"10");
+        --    apple_x <= (apple_coord / 4d"10");
 
-            apple_id <= '1' & apple_x & apple_y;
+        --     apple_id <= '1' & apple_x & apple_y;
         end if;
     end process;
 
