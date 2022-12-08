58f3d83227cca7473c3a09a94858e4f26609dd2a
xertyx <ethan197019@gmail.com>
2022-12-08 03:22:23 -0500

    test

--- ES4_Snake/source/impl_1/display.vhd
+++ ES4_Snake/source/impl_1/display.vhd
@@ -83,7 +83,7 @@ begin
     -- 9B"1_1000_0111" represents the random apple. "1" means that it exists, "1000" means that the column number is 8, "0111" means that the row number is 7
     --snake_loc <= ("0000000000000000001000000000100000000010111111111010000000000000000000000000000000000000000000000000"); 
     --pattern_gen_init: pattern_gen port map(valid, row_cnt, column_cnt, 9B"1_1000_0111", snake_loc, rgb);
-    pattern_gen_initial: pattern_gen port map(valid, row_cnt, column_cnt, apple, snake, "000010", intermed_rgb);
+    pattern_gen_initial: pattern_gen port map(valid, row_cnt, column_cnt, apple, snake, scores, intermed_rgb);
     segments: eight_segments port map(row_cnt, column_cnt, clk, score_rgb);
 
     rgb <= intermed_rgb when (column_cnt > 100 and valid = '1') else 
