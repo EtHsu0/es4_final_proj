3acac8de8f2b024efc2e75445d6b79344e526133
xertyx <ethan197019@gmail.com>
2022-12-07 22:03:47 -0500

    Test

--- ES4_Snake/source/impl_1/snakePos.vhd
+++ ES4_Snake/source/impl_1/snakePos.vhd
@@ -80,8 +80,8 @@ begin
             snake_coord := snake_head;
             snake_arr(to_integer(snake_coord)) <= '1';
 
-            for i in 0 to to_integer(snake_len_in) loop
-            --   exit when i = to_integer(snake_len_in);
+            for i in 0 to 99 loop
+              exit when i = to_integer(snake_len_in);
               case snake_dir_arr(i) is
                  when UP => snake_coord := snake_coord - 10;
                  when DOWN => snake_coord := snake_coord + 10;
